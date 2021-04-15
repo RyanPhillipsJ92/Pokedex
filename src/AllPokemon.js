3af6b8191a8cda85557891fae02b987cb96b86d4
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-04-15 12:27:42 -0500

    pokedex will render all 151 original pokemon

--- src/AllPokemon.js
+++ src/AllPokemon.js
@@ -1,79 +1,83 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import './App.css';
+
+
 const AllPokemon = () => {
     const [pokemon, setPokemon] = useState("");
     const [pokeData, setData] = useState([]);
     const [pokeType, setType] = useState("");
 
+
     const getPokemon = async () => {
         const toArray = [];
         try {
-            const url = `https://pokeapi.co/api/v2/pokemon/?limit=150`;
+            const url = `https://pokeapi.co/api/v2/pokemon/?limit=151`;
             const res = await axios.get(url);
-            for (let i = 0; i <= res.data.results.length; i++){
-
+            for (let i = 0; i <= res.data.results.length; i++) {
                 let name = res.data.results[i].name;
-
                 const urlAll = `https://pokeapi.co/api/v2/pokemon/${name}`;
                 const resAll = await axios.get(urlAll);
+                toArray.push(resAll.data);
+                setType(resAll.data.types[0].type.name)
+                setData(toArray)
+
                 console.log("Name: " + resAll.data.name +
                     ", Height: " + Math.round(resAll.data.height * 3.9) +
                     ", Weight: " + Math.round(resAll.data.weight / 4.3))
 
-                    // let PokemonName = resAll.data.data.results.name;
-                    // let PokemonWeight = Math.round(resAll.data.results[i].weight / 4.3);
-                    // let PokemonType = resAll.data.results[i].type;
-                    // let PokemonIdNumber = resAll.data.results[i].id;
-                    // let PokemonHeight = Math.round(resAll.data.results[i].height* 3.9);
-                    // console.log(PokemonName ,PokemonWeight, PokemonHeight, PokemonType, PokemonIdNumber)
-                    //     console.log(PokemonName)
-                // console.log(name, resAll.data.type)
-
-                // console.log(resAll.data)
             }
             // console.log(res)
         } catch (e) {
             console.log(e)
         }
     };
+
+    const capFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
     useEffect(() => {
-        getPokemon();
+        getPokemon().then((data) => {
+            setPokemon(data)
+        });
+
     }, []);
-    return (
-        <div className="App">
-            {/*<form onSubmit={handleSubmit}>*/}
-            {/*    <label>*/}
-            {/*        <input type="text" onChange={handleChange} placeholder="Enter Pokemon Name"/>*/}
-            {/*    </label>*/}
-            {/*</form>*/}
+    console.log(pokemon)
 
-            {/*{pokeData.map((data) => {*/}
-            {/*    return (*/}
-            {/*        <div className="container">*/}
-            {/*            <img src={data.sprites["front_default"]}/>*/}
-            {/*            <div className="divTable">*/}
-            {/*                <div className="divTableBody"></div>*/}
+        return (
+            <div className="App">
+                <div className="container-cards container-fluid">
+                    <div className="card">
+                        <div className="card-header text-center">
+                            The Original 150 Pokémon
+                        </div>
+                        <div className="card-body">
+                            {pokeData.map((data) => {
+                                return (
+                                    <div className="container-fluid">
+                                        <div className="row">
+                                            <div className="col-12 col-md-6 col-lg-4" id="pkmn-srch">
+                                                <div className="card" id="pkmn">
+                                                    <img className="card-img-top" src={data.sprites["front_default"]}
+                                                         alt={capFirstLetter(data.name)}/>
+                                                    <div className="card-body">
+                                                        <h3 className="card-title">{" "}{capFirstLetter(data.name)}</h3>
+                                                        <p className="card-text">Pokédex #{" "}{data.id}</p>
+                                                        <p className="card-text">Type: {" "}{capFirstLetter(pokeType)}</p>
+                                                        <p className="card-text">Height: {" "}{Math.round(data.height * 3.9)}"</p>
+                                                        <p className="card-text">Weight: {" "}{Math.round(data.weight / 4.3)} lbs</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    export default AllPokemon;
 
-            {/*                <div className="divTableRow">*/}
-            {/*                    <div className="divTableCell">Type</div>*/}
-            {/*                    <div className="divTableCell">{pokeType.toUpperCase()}</div>*/}
-            {/*                </div>*/}
-
-            {/*                <div className="divTableRow">*/}
-            {/*                    <div className="divTableCell">Height</div>*/}
-            {/*                    <div className="divTableCell">{" "}{Math.round(data.height * 3.9)} "</div>*/}
-            {/*                </div>*/}
-
-            {/*                <div className="divTableRow">*/}
-            {/*                    <div className="divTableCell">Weight</div>*/}
-            {/*                    <div className="divTableCell">{" "}{Math.round(data.weight / 4.3)} lbs</div>*/}
-            {/*                </div>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    );*/}
-            {/*})}*/}
-        </div>
-    );
-};
-export default AllPokemon;
